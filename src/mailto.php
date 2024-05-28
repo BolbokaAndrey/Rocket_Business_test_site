d29694fbc83bb3052e53b49e2281fef50caba05e
Андрей <bolbocka@yandex.ru>
2024-05-28 13:43:58 +0300

    Add mailto.php

--- src/mailto.php
+++ src/mailto.php
@@ -1,27 +1,14 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+$to = 'rbru-metrika@yandex.ru';
+$tema = "Фидбэк от пользователя ".$_POST['name'];
+$message = $_POST['message'];
+$headers = 'From: '.$_POST['email']."\r\n";
 
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/Exception.php';
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('ru', 'phpmailer/language/');
-$mail->isHTML(true);
-try {
-
-    $mail->setFrom($_POST['email']);
-    $mail->addAddress('rbru-metrika@yandex.ru');
-
-    $mail->Subject = "Фидбэк от пользователя ".$_POST['name'];
-    $mail->Body    = $_POST['message'];
-
-    $mail->send();
+if (mail($to, $tema, $message, $headers)) {
     $refer = $_SERVER["HTTP_REFERER"];
     header('Location: '.$refer);
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+else {
+    echo "Функция mail() не работает на бесплатном хостинге";
 }
